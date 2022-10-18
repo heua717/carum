7a416b0580939fdef26e7776edbeff5fa115a492
surin kwon <tnfls7070@naver.com>
2022-10-18 18:15:16 +0900

    [CHORE] organize react project folder structure

--- frontend/carum/src/App.js
+++ frontend/carum/src/App.js
@@ -2,7 +2,15 @@ import "./App.css";
 import { BrouserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrouserRouter>
+        <Routes>
+          <Route path="/" />
+        </Routes>
+      </BrouserRouter>
+    </div>
+  );
 }
 
 export default App;
