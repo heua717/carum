0db4a98c37a0333947f90d48652c252191e6af5a
gnusho <osj2387@naver.com>
2022-10-18 12:57:56 +0900

    [chore] create backend project

---
+++ backend/src/main/java/com/a101/carum/CarumApplication.java
@@ -0,0 +1,13 @@
+package com.a101.carum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarumApplication.class, args);
+	}
+
+}
