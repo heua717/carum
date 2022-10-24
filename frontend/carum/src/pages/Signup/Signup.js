b1fad8f885179fc5a3a06efcd268a6d511cf21ee
surin kwon <tnfls7070@naver.com>
2022-10-24 17:56:34 +0900

    [FEAT] modify signup component 회원가입 페이지 구조 잡았습니다.

--- frontend/carum/src/pages/Signup/Signup.js
+++ frontend/carum/src/pages/Signup/Signup.js
@@ -1,5 +1,26 @@
+import TextField from "@mui/material/TextField";
+import Checkbox from "@mui/material/Checkbox";
+import { DesktopDatePicker } from "@mui/x-date-pickers";
+import Button from "../../components/Button";
+
 function Signup() {
-  return <div></div>;
+  return (
+    <div>
+      <div>
+        <span>회원가입</span>
+      </div>
+      <TextField required label="아이디" id="outlined-required" />
+      <TextField />
+      <DesktopDatePicker />
+      <TextField />
+      <TextField />
+      <div>
+        <Checkbox />
+        <Button size={"medium"} variant={"light"} text={"휴대폰 인증"} />
+      </div>
+      <Button size={"big"} variant={"primary"} text={"가입"} />
+    </div>
+  );
 }
 
 export default Signup;
