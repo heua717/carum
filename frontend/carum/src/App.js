53291a77c516fc1a3cb9081f67febaf776106fc8
surin kwon <tnfls7070@naver.com>
2022-10-25 13:36:10 +0900

    [FEAT] modify App.js router and add css

--- frontend/carum/src/App.js
+++ frontend/carum/src/App.js
@@ -1,10 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Login from "./pages/Login/Login";
+import Signup from "./pages/Signup/Signup";
+import Main from "./pages/Main/Main";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" />
+        <Route path="/" element={<Login />} />
+        <Route path="/main/*" element={<Main />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </Router>
   );
