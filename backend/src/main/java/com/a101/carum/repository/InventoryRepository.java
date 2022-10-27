e892a436a2381f35e5513227aad759f398b2a7d6
gnusho <osj2387@naver.com>
2022-10-27 15:17:32 +0900

    [FEAT] create inventory entity, repository, service, controller

---
+++ backend/src/main/java/com/a101/carum/repository/InventoryRepository.java
@@ -0,0 +1,9 @@
+package com.a101.carum.repository;
+
+import com.a101.carum.domain.inventory.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+}
---
+++ backend/src/main/java/com/a101/carum/service/InventoryService.java
@@ -0,0 +1,13 @@
+package com.a101.carum.service;
+
+import com.a101.carum.repository.InventoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class InventoryService {
+
+    private final InventoryRepository inventoryRepository;
+
+}
