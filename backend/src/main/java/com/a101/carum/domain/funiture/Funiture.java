46501c4b6c01136f44318d89a17954e35c0cd204
gnusho <osj2387@naver.com>
2022-10-27 11:06:15 +0900

    [CHORE] update application.properties

---
+++ backend/src/main/java/com/a101/carum/domain/funiture/Funiture.java
@@ -0,0 +1,36 @@
+package com.a101.carum.domain.funiture;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor
+@DynamicInsert
+@DynamicUpdate
+@Getter
+@ToString
+public class Funiture {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", columnDefinition = "BIGINT(20) UNSIGNED")
+    private Long id;
+
+    @Column(name = "name", nullable = false, unique = true)
+    private String name;
+
+    @Column(name = "resource", nullable = false, unique = true)
+    private String resource;
+
+    @Column(name = "price", nullable = false)
+    private Integer price = 0;
+
+    @Column(name = "type", length = 20)
+    @Enumerated(EnumType.STRING)
+    private FunitureType type;
+}
