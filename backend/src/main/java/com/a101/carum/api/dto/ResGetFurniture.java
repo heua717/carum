a38c700b5e52476a736e0c8ed2aa63d108169371
gnusho <osj2387@naver.com>
2022-10-27 15:06:22 +0900

    [FEAT] temporary read furnitures in shop api

--- backend/src/main/java/com/a101/carum/api/dto/ResGetFurniture.java
+++ backend/src/main/java/com/a101/carum/api/dto/ResGetFurniture.java
@@ -4,20 +4,14 @@ import com.a101.carum.domain.furniture.FurnitureType;
 import lombok.*;
 
 @Data
+@Builder
+@AllArgsConstructor
 @NoArgsConstructor
 public class ResGetFurniture {
-    Long furnitureId;
+    Long id;
     String name,resource;
     Integer price;
     FurnitureType type;
     boolean have;
 
-    public void ResGetFurnitureList(Long id, String name, String resource, Integer price, FurnitureType type, boolean have){
-        this.furnitureId = id;
-        this.name = name;
-        this.resource = resource;
-        this.price = price;
-        this.type = type;
-        this.have = have;
-    }
 }
