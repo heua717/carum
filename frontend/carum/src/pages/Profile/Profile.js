c2bc63e21889f0e6c167f129b9c17df9edae8c2b
surin kwon <tnfls7070@naver.com>
2022-10-27 09:33:21 +0900

    [FEAT] add profile page

--- frontend/carum/src/pages/Profile/Profile.js
+++ frontend/carum/src/pages/Profile/Profile.js
@@ -1,7 +1,59 @@
 import styles from "./Profile.module.css";
+import TopNav from "../../components/TopNav";
+import Button from "../../components/Button";
+import { TextField } from "@mui/material";
+import { useState } from "react";
+import EditIcon from "@material-ui/icons/Edit";
 
 function Profile() {
-  return <div>내정보</div>;
+  const [values, setValues] = useState({
+    userInfo: "",
+    isEditing: false,
+    newNickname: "",
+    newPassword: "",
+    newPasswordConfirm: "",
+  });
+
+  const editNickname = () => {
+    setValues({ ...values, isEditing: false });
+  };
+
+  return (
+    <div className={styles.container}>
+      <TopNav text="내 정보" />
+      <p className={styles.id}>아이디</p>
+      <p className={styles.settingTag}>닉네임</p>
+      {values.isEditing ? (
+        <div className={styles.nicknameEditbox}>
+          <TextField className={styles.inputBox} />
+          <div className={styles.editButton}>
+            <Button
+              text="닉네임 수정"
+              variant="primary"
+              size="small"
+              onClick={() => editNickname()}
+            />
+          </div>
+        </div>
+      ) : (
+        <div className={styles.nicknameSettingBox}>
+          <span className={styles.nickname}>닉네임</span>
+          <EditIcon onClick={() => setValues({ ...values, isEditing: true })} />
+        </div>
+      )}
+      <div>
+        <p className={styles.settingTag}>새 비밀번호</p>
+        <TextField className={styles.inputBox} />
+      </div>
+      <div>
+        <p className={styles.settingTag}>새 비밀번호 확인</p>
+        <TextField className={styles.inputBox} />
+      </div>
+      <div className={styles.editButton}>
+        <Button text="비밀번호 수정" variant="primary" size="small" />
+      </div>
+    </div>
+  );
 }
 
 export default Profile;
