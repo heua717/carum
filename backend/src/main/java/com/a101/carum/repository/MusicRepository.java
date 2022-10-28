5a905c05b76514f5d2588a4b74063d2fe5932da3
gnusho <osj2387@naver.com>
2022-10-28 16:15:51 +0900

    [FEAT] create room, interior, music, playlist repository

--- backend/src/main/java/com/a101/carum/repository/MusicRepository.java
+++ backend/src/main/java/com/a101/carum/repository/MusicRepository.java
@@ -1,10 +1,10 @@
 package com.a101.carum.repository;
 
-import com.a101.carum.domain.furniture.Furniture;
-import com.a101.carum.domain.room.Room;
+import com.a101.carum.domain.interior.Interior;
+import com.a101.carum.domain.music.Music;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RoomRepository extends JpaRepository<Room, Long> {
+public interface MusicRepository extends JpaRepository<Music, Long> {
 }
