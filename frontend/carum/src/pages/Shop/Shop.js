a04e4c4f12c3c7da471463f6e82b52da21327ee0
surin kwon <tnfls7070@naver.com>
2022-11-03 17:27:39 +0900

    [FEAT] add and modify inventory

--- frontend/carum/src/pages/Shop/Shop.js
+++ frontend/carum/src/pages/Shop/Shop.js
@@ -12,7 +12,7 @@ import CloseIcon from "@material-ui/icons/Close";
 import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
 import { furnitureCategory } from "utils/utils";
-import { fetchShopItem, purchaseFurniture } from "apis/furniture";
+import { fetchShopItem, purchaseFurniture, fetchMyItem } from "apis/furniture";
 
 function Shop() {
   const [place, setPlace] = useState("shop");
@@ -46,14 +46,16 @@ function Shop() {
 
   //페이지 이동 시, 카테고리 변경 시 가구 검색
   useEffect(() => {
-    const payload = {
-      keyword: searchText ? searchText : null,
-      type: furnitureCategory[categoryIndex].type,
-      page: page - 1,
-      size: 4,
-    };
+    if (place === "shop") {
+      const payload = {
+        keyword: searchText ? searchText : null,
+        type: furnitureCategory[categoryIndex].type,
+        page: page - 1,
+        size: 4,
+      };
 
-    fetchShopItem(payload, fetchShopItemSuccess, fetchShopItemFail);
+      fetchShopItem(payload, fetchShopItemSuccess, fetchShopItemFail);
+    }
   }, [page]);
 
   useEffect(() => {
@@ -80,12 +82,32 @@ function Shop() {
     }
   };
 
+  const fetchMyItemSuccess = (res) => {
+    console.log(res.data);
+    setPage(1);
+    setCategoryIndex(0);
+    setFurnitureList(res.data.furnitureList);
+  };
+
+  const fetchMyItemFail = (err) => {
+    console.log(err);
+  };
+
   // 가구점, 내 가구 이동
   const movePlace = () => {
     if (place === "myRoom") {
       setPlace("shop");
+      const payload = {
+        keyword: null,
+        type: furnitureCategory[categoryIndex].type,
+        page: page - 1,
+        size: 4,
+      };
+
+      fetchShopItem(payload, fetchShopItemSuccess, fetchShopItemFail);
     } else {
       setPlace("myRoom");
+      fetchMyItem(fetchMyItemSuccess, fetchMyItemFail);
     }
   };
 
@@ -148,100 +170,170 @@ function Shop() {
           />
         }
       />
-      <div className={styles.searchBox}>
-        <MenuIcon onClick={handleMenuClick} id="menu-bth" />
-        <Menu
-          anchorEl={anchorEl}
-          open={isMenuOpened}
-          onClose={handleMenuClose}
-          MenuListProps={{ "aria-labelledby": "menu-btn" }}
-          sx={{ maxHeight: "200px" }}
-        >
-          {furnitureCategory.map((el, idx) => {
-            return (
-              <MenuItem onClick={() => handleMenuClose(idx)} key={idx}>
-                {el.name}
-              </MenuItem>
-            );
-          })}
-        </Menu>
-        <input
-          className={styles.inputBox}
-          value={searchText}
-          onChange={(e) => setSearchText(e.target.value)}
-        />
-        <SearchIcon onClick={handleFurnitureSearch} />
-      </div>
-      <div className={styles.contentBox}>
-        <p className={styles.categoryName}>
-          {furnitureCategory[categoryIndex].name}
-        </p>
-        <div className={styles.furnitureRow}>
-          {furnitureList?.map((el, idx) => {
-            if (idx < 2) {
-              return (
-                <FurnitureComponent
-                  name={el.name}
-                  have={el.have}
-                  place={place}
-                  point={el.price}
-                  onClick={() => handleOpen(idx)}
-                  key={idx}
-                />
-              );
-            }
-          })}
-        </div>
-        <div className={styles.furnitureRow}>
-          {furnitureList?.map((el, idx) => {
-            if (idx > 1) {
-              return (
-                <FurnitureComponent
-                  name={el.name}
-                  have={el.have}
-                  place={place}
-                  point={el.price}
-                  onClick={() => handleOpen(idx)}
-                  key={idx}
-                />
-              );
-            }
-          })}
-        </div>
-        <Pagination
-          size="small"
-          count={totalPage}
-          className={styles.pagination}
-          onChange={handlePageChange}
-          defaultPage={1}
-          page={page}
-        />
-      </div>
-      {/* 가구 dialog */}
-      <Dialog className={styles.dialog} open={isOpened} onClose={handleClose}>
-        <div className={styles.dialog}>
-          <CloseIcon onClick={handleClose} className={styles.closeBtn} />
-          <DialogTitle>
-            {furnitureList[currentFurnitureIndex]?.name}
-          </DialogTitle>
-          <div className={styles.furniture}>가구</div>
-          <p className={styles.detailPriceTag}>
-            {furnitureList[currentFurnitureIndex]?.price}
-          </p>
-          <div className={styles.myPointBox}>
-            <p className={styles.myPointText}>내 포인트</p>
-            <p className={styles.myPointText}>{money} point</p>
+      {place === "shop" ? (
+        <div>
+          <div className={styles.searchBox}>
+            <MenuIcon onClick={handleMenuClick} id="menu-bth" />
+            <Menu
+              anchorEl={anchorEl}
+              open={isMenuOpened}
+              onClose={handleMenuClose}
+              MenuListProps={{ "aria-labelledby": "menu-btn" }}
+              sx={{ maxHeight: "200px" }}
+            >
+              {furnitureCategory.map((el, idx) => {
+                return (
+                  <MenuItem onClick={() => handleMenuClose(idx)} key={idx}>
+                    {el.name}
+                  </MenuItem>
+                );
+              })}
+            </Menu>
+            <input
+              className={styles.inputBox}
+              value={searchText}
+              onChange={(e) => setSearchText(e.target.value)}
+            />
+            <SearchIcon onClick={handleFurnitureSearch} />
           </div>
-          <Button
-            text="구매"
-            variant="light"
+          <div className={styles.contentBox}>
+            <p className={styles.categoryName}>
+              {furnitureCategory[categoryIndex].name}
+            </p>
+            <div className={styles.furnitureRow}>
+              {furnitureList?.map((el, idx) => {
+                if (idx < 2) {
+                  return (
+                    <FurnitureComponent
+                      name={el.name}
+                      have={el.have}
+                      place={place}
+                      point={el.price}
+                      onClick={() => handleOpen(idx)}
+                      key={idx}
+                    />
+                  );
+                }
+              })}
+            </div>
+            <div className={styles.furnitureRow}>
+              {furnitureList?.map((el, idx) => {
+                if (idx > 1) {
+                  return (
+                    <FurnitureComponent
+                      name={el.name}
+                      have={el.have}
+                      place={place}
+                      point={el.price}
+                      onClick={() => handleOpen(idx)}
+                      key={idx}
+                    />
+                  );
+                }
+              })}
+            </div>
+            <Pagination
+              size="small"
+              count={totalPage}
+              className={styles.pagination}
+              onChange={handlePageChange}
+              defaultPage={1}
+              page={page}
+            />
+          </div>
+          {/* 가구 dialog */}
+          <Dialog
+            className={styles.dialog}
+            open={isOpened}
+            onClose={handleClose}
+          >
+            <div className={styles.dialog}>
+              <CloseIcon onClick={handleClose} className={styles.closeBtn} />
+              <DialogTitle>
+                {furnitureList[currentFurnitureIndex]?.name}
+              </DialogTitle>
+              <div className={styles.furniture}>가구</div>
+              <p className={styles.detailPriceTag}>
+                {furnitureList[currentFurnitureIndex]?.price}
+              </p>
+              <div className={styles.myPointBox}>
+                <p className={styles.myPointText}>내 포인트</p>
+                <p className={styles.myPointText}>{money} point</p>
+              </div>
+              <Button
+                text="구매"
+                variant="light"
+                size="small"
+                onClick={() =>
+                  handlePurchaseFurniture(
+                    furnitureList[currentFurnitureIndex]?.id
+                  )
+                }
+              />
+            </div>
+          </Dialog>
+        </div>
+      ) : (
+        <MyRoomComponent furnitureList={furnitureList} place={place} />
+      )}
+    </div>
+  );
+}
+
+function MyRoomComponent({ furnitureList, place }) {
+  const [newPage, setNewPage] = useState(1);
+  const [newTotalPage, setNewTotalPage] = useState(
+    furnitureList.length ? furnitureList.length : 0
+  );
+
+  const handleInventoryPage = (e, p) => {
+    setNewPage(p);
+  };
+
+  return (
+    <div>
+      {furnitureList.length > 0 ? (
+        <div className={styles.contentBox}>
+          <div className={styles.furnitureRow}>
+            {furnitureList[(newPage - 1) * 4] ? (
+              <FurnitureComponent
+                name={furnitureList[(newPage - 1) * 4]?.name}
+                place={place}
+              />
+            ) : null}
+            {furnitureList[(newPage - 1) * 4 + 1] ? (
+              <FurnitureComponent
+                name={furnitureList[(newPage - 1) * 4 + 1]?.name}
+                place={place}
+              />
+            ) : null}
+          </div>
+          <div className={styles.furnitureRow}>
+            {furnitureList[(newPage - 1) * 4 + 2] ? (
+              <FurnitureComponent
+                name={furnitureList[(newPage - 1) * 4 + 2]?.name}
+                place={place}
+              />
+            ) : null}
+            {furnitureList[(newPage - 1) * 4 + 3] ? (
+              <FurnitureComponent
+                name={furnitureList[(newPage - 1) * 4 + 3]?.name}
+                place={place}
+              />
+            ) : null}
+          </div>
+          <Pagination
             size="small"
-            onClick={() =>
-              handlePurchaseFurniture(furnitureList[currentFurnitureIndex]?.id)
-            }
+            count={newTotalPage}
+            className={styles.pagination}
+            onChange={handleInventoryPage}
+            defaultPage={1}
+            page={newPage}
           />
         </div>
-      </Dialog>
+      ) : (
+        <p className={styles.inventoryNoDataText}>가구가 없습니다.</p>
+      )}
     </div>
   );
 }
