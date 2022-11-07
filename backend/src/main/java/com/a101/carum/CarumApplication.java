02f4828ae054dce581da1b31bade52575d197ffe
gnusho <osj2387@naver.com>
2022-11-07 14:54:22 +0900

    [CHORE] set schedular

--- backend/src/main/java/com/a101/carum/CarumApplication.java
+++ backend/src/main/java/com/a101/carum/CarumApplication.java
@@ -2,8 +2,10 @@ package com.a101.carum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 
+@EnableScheduling
 @SpringBootApplication
 public class CarumApplication {
 	public static void main(String[] args) {
---
+++ backend/src/main/java/com/a101/carum/util/Scheduler.java
@@ -0,0 +1,9 @@
+package com.a101.carum.util;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Scheduler {
+
+}
