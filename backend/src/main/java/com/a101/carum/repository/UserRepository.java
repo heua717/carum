4fb23ebe5a4140649feb5f07169e14979f2e5296
gnusho <osj2387@naver.com>
2022-11-08 17:21:27 +0900

    [FEAT] add monthly pet schedular

--- backend/src/main/java/com/a101/carum/repository/UserRepository.java
+++ backend/src/main/java/com/a101/carum/repository/UserRepository.java
@@ -4,6 +4,7 @@ import com.a101.carum.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -15,4 +16,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     User findByUserIdAndIsDeleted(String userId, boolean b);
 
     User findByNickNameAndIsDeleted(String nickName, boolean b);
+
+    List<User> findAllByIsDeleted(boolean b);
 }
