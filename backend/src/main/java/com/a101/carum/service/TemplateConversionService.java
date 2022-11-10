8ef623bfdf5aaf5fa725eefea7fb742cdb8538aa
gnusho <osj2387@naver.com>
2022-11-10 10:21:15 +0900

    [REFACTOR] create room

--- backend/src/main/java/com/a101/carum/service/TemplateConversionService.java
+++ backend/src/main/java/com/a101/carum/service/TemplateConversionService.java
@@ -4,13 +4,12 @@ import com.a101.carum.api.dto.ReqPostRoom;
 import com.a101.carum.domain.room.RoomParent;
 import com.a101.carum.domain.room.RoomTemplate;
 import com.a101.carum.domain.user.User;
-import com.a101.carum.repository.RoomParentRepository;
-import com.a101.carum.repository.RoomRepository;
-import com.a101.carum.repository.RoomTemplateRepository;
+import com.a101.carum.repository.*;
 import com.a101.carum.util.RoomParentFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.*;
 
@@ -22,6 +21,8 @@ public class TemplateConversionService {
     private final RoomParentRepository roomParentRepository;
     private final RoomRepository roomRepository;
     private final RoomTemplateRepository roomTemplateRepository;
+    private final InteriorRepository interiorRepository;
+    private final PlaylistRepository playlistRepository;
 
     @Value("${room.template.base}")
     private Long TEMPLATE_BASE;
@@ -38,11 +39,19 @@ public class TemplateConversionService {
         }
     }
 
-
+    @Transactional
     public void createBaseRoom(User user, ReqPostRoom reqPostRoom) {
         createRoomParent(user, reqPostRoom.getName(), TEMPLATE_BASE);
     }
 
+    @Transactional
+    public void createNewRoomAll(User user) {
+        for(Long templateId: TEMPLATE_LIST){
+            createRoomParent(user, null, templateId);
+        }
+    }
+
+    @Transactional
     public void createRoomParent(User user, String name, Long templateId){
         RoomTemplate roomTemplate = roomTemplateRepository.findById(templateId)
                 .orElseThrow(() -> {throw new NullPointerException("템플릿이 없습니다.");});
@@ -53,6 +62,20 @@ public class TemplateConversionService {
                 roomTemplate.getFrame(),
                 roomTemplate.getEmotionTag()
         );
+        roomParentRepository.save(roomParent);
+    }
 
+    @Transactional
+    public void setRoomDetail(RoomParent roomParent, RoomTemplate roomTemplate){
+        setInteriors(roomParent, roomTemplate);
+        setPlaylists(roomParent, roomTemplate);
+    }
+
+    @Transactional
+    public void setPlaylists(RoomParent roomParent, RoomTemplate roomTemplate) {
+    }
+
+    @Transactional
+    public void setInteriors(RoomParent roomParent, RoomTemplate roomTemplate) {
     }
 }
